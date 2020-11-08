869a3f8ccda1877ae76acedf941685eb08538fb3
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-11-08 18:17:40 +0900

    アソシエーションの作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,6 +25,7 @@ class BlogsController < ApplicationController
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
 
     respond_to do |format|
       if @blog.save
